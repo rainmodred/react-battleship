decfc9574f3adcbb3d713f71b302922370516b00
rainmodred <albphoenix@gmail.com>
2020-02-22 16:57:56 +0300

    Updates

---
+++ precache-manifest.4e9b8754baaa4c75c1ca388624a27999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c8e62f9f00c7d4587b48d58b11c94e",
+    "url": "/react-battleship/index.html"
+  },
+  {
+    "revision": "299c6f694a5a8fd8c341",
+    "url": "/react-battleship/static/js/2.9cacbff0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-battleship/static/js/2.9cacbff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c1599df93e71e054a3",
+    "url": "/react-battleship/static/js/main.b3343817.chunk.js"
+  },
+  {
+    "revision": "99e4e3bd24b6313a62f1",
+    "url": "/react-battleship/static/js/runtime-main.8ee23db5.js"
+  }
+]);
